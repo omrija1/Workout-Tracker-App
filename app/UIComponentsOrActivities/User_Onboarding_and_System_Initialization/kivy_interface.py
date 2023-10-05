0df757f5beacc546048a457f72eaf25f6ecbdfa5
omrija1 <142934086+omrija1@users.noreply.github.com>
2023-10-05 14:01:57 +0300

    new screens added with navigaiton

--- app/UIComponentsOrActivities/User_Onboarding_and_System_Initialization/kivy_interface.py
+++ app/UIComponentsOrActivities/User_Onboarding_and_System_Initialization/kivy_interface.py
@@ -273,6 +273,7 @@ class MainDashboard(BaseScreen):
     
     Methods:
         init_ui: Initialize the user interface.
+        navigate_to_screen: Navigate to a specified screen.
     """
     
     def __init__(self, **kwargs):
@@ -288,7 +289,30 @@ class MainDashboard(BaseScreen):
     def init_ui(self):
         """Initialize the UI elements for the main dashboard."""
         self.add_widget_to_layout(Label(text='Welcome to the Main Dashboard!', size_hint_y=None, height=30, font_size=24))
+        self.add_widget_to_layout(Button(text='Go to Screen 1', size_hint_y=None, height=50, on_press=self.navigate_to_screen('screen_1')))
+        self.add_widget_to_layout(Button(text='Go to Screen 2', size_hint_y=None, height=50, on_press=self.navigate_to_screen('screen_2')))
 
+    def navigate_to_screen(self, screen_name):
+        """
+        Navigate to the specified screen.
+        
+        Args:
+            screen_name (str): The name of the screen to navigate to.
+        """
+        def inner_function(instance):
+            self.manager.current = screen_name
+        return inner_function
+
+# New Screen1 and Screen2 classes
+class Screen1(BaseScreen):
+    def __init__(self, **kwargs):
+        super(Screen1, self).__init__(**kwargs)
+        self.add_widget_to_layout(Label(text='This is Screen 1', size_hint_y=None, height=30, font_size=24))
+
+class Screen2(BaseScreen):
+    def __init__(self, **kwargs):
+        super(Screen2, self).__init__(**kwargs)
+        self.add_widget_to_layout(Label(text='This is Screen 2', size_hint_y=None, height=30, font_size=24))
 
 class Manager(ScreenManager):
     """
@@ -315,6 +339,8 @@ class Manager(ScreenManager):
         self.add_widget(RegisterScreen(name='register'))
         self.add_widget(LoginScreen(name='login'))
         self.add_widget(MainDashboard(name='main_dashboard'))
+        self.add_widget(Screen1(name='screen_1'))
+        self.add_widget(Screen2(name='screen_2'))
 class MyApp(App):
     """
     Main application class.
